4085d6d958b29a58a4a82e0f1caa1fed44ce2fe2
hirokazu.kobayashi <hirokazu.kobayashi.koba.hiro@gmail.com>
2025-02-28 09:00:39 +0900

    update design 6

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,14 +1,14 @@
 import {
-  Avatar,
-  Button,
-  Container,
-  Divider,
-  InputAdornment,
-  Link,
-  Paper,
-  Stack,
-  TextField,
-  Typography,
+    Box,
+    Button,
+    Container,
+    Divider,
+    InputAdornment,
+    Link,
+    Paper,
+    Stack,
+    TextField,
+    Typography,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
@@ -115,19 +115,14 @@ export default function SignIn() {
   if (!data) return <Loading />;
   if (data && data.session_enabled) return <Loading />;
 
-  return (
-    <>
-      <Container maxWidth={isMobile ? "xs" : "sm"}>
-        <Paper sx={{ p: isMobile ? 2 : 3, boxShadow: 1, borderRadius: 3 }}>
-          <Stack spacing={isMobile ? 2 : 3} alignItems="center">
+  const content = (
+      <Stack spacing={isMobile ? 2 : 3} alignItems="center">
 
-            <Avatar src={data.logo_uri} sx={{ width: 56, height: 56 }} />
+          <Typography variant="h6" fontWeight="medium">
+              IdP Server
+          </Typography>
 
-            <Typography variant="subtitle1" fontWeight="medium">
-              {data.client_name}
-            </Typography>
-
-            <Stack spacing={2} width="100%">
+          <Stack spacing={2} width="100%">
               <TextField
                   name="email"
                   placeholder="Your email"
@@ -136,13 +131,13 @@ export default function SignIn() {
                   required
                   variant="standard"
                   InputProps={{
-                    disableUnderline: true,
-                    sx: { height: 48, fontSize: 17, backgroundColor: "#F5F5F7", borderRadius: 2, px: 2 },
-                    startAdornment: (
-                        <InputAdornment position="start">
-                          <Email sx={{ color: "gray" }} />
-                        </InputAdornment>
-                    ),
+                      disableUnderline: true,
+                      sx: { height: 48, fontSize: 17, backgroundColor: "#F5F5F7", borderRadius: 2, px: 2 },
+                      startAdornment: (
+                          <InputAdornment position="start">
+                              <Email sx={{ color: "gray" }} />
+                          </InputAdornment>
+                      ),
                   }}
                   onChange={(e) => setEmail(e.target.value)}
               />
@@ -154,102 +149,115 @@ export default function SignIn() {
                   required
                   variant="standard"
                   InputProps={{
-                    disableUnderline: true,
-                    sx: { height: 48, fontSize: 17, backgroundColor: "#F5F5F7", borderRadius: 2, px: 2 },
-                    startAdornment: (
-                        <InputAdornment position="start">
-                          <Lock sx={{ color: "gray" }} />
-                        </InputAdornment>
-                    ),
+                      disableUnderline: true,
+                      sx: { height: 48, fontSize: 17, backgroundColor: "#F5F5F7", borderRadius: 2, px: 2 },
+                      startAdornment: (
+                          <InputAdornment position="start">
+                              <Lock sx={{ color: "gray" }} />
+                          </InputAdornment>
+                      ),
                   }}
                   onChange={(e) => {
-                    navigator.credentials.preventSilentAccess();
-                    setPassword(e.target.value);
+                      navigator.credentials.preventSilentAccess();
+                      setPassword(e.target.value);
                   }}
               />
-            </Stack>
+          </Stack>
 
-            <Stack spacing={1} width="100%">
+          <Stack spacing={1} width="100%">
               <Typography variant="body2" textAlign="center" color="text.secondary">
-                By signing in, you agree to our
+                  By signing in, you agree to our
               </Typography>
               <Stack direction="column" alignItems="center">
-                <Link
-                    href={data.tos_uri}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    sx={{
-                      textDecoration: "none",
-                      color: "primary.main",
-                      fontWeight: "bold",
-                      textAlign: "center",
-                      display: "block",
-                      minHeight: 44,
-                      lineHeight: "44px",
-                    }}
-                >
-                  Terms of Use
-                </Link>
-                <Link
-                    href={data.policy_uri}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    sx={{
-                      textDecoration: "none",
-                      color: "primary.main",
-                      fontWeight: "bold",
-                      textAlign: "center",
-                      display: "block",
-                      minHeight: 44,
-                      lineHeight: "44px",
-                    }}
-                >
-                  Privacy Policy
-                </Link>
+                  <Link
+                      href={data?.tos_uri}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      sx={{
+                          textDecoration: "none",
+                          color: "primary.main",
+                          fontWeight: "bold",
+                          textAlign: "center",
+                          display: "block",
+                          minHeight: 44,
+                          lineHeight: "44px",
+                      }}
+                  >
+                      Terms of Use
+                  </Link>
+                  <Link
+                      href={data?.policy_uri}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      sx={{
+                          textDecoration: "none",
+                          color: "primary.main",
+                          fontWeight: "bold",
+                          textAlign: "center",
+                          display: "block",
+                          minHeight: 44,
+                          lineHeight: "44px",
+                      }}
+                  >
+                      Privacy Policy
+                  </Link>
               </Stack>
-            </Stack>
+          </Stack>
 
-            <Stack spacing={1} width="100%">
+          <Stack spacing={1} width="100%">
               <Button
                   variant="contained"
                   disabled={!email || !password}
                   onClick={handleNext}
                   fullWidth
                   sx={{
-                    textTransform: "none",
-                    borderRadius: 10,
-                    height: 50,
-                    fontSize: 17,
-                    fontWeight: "bold",
-                    backgroundColor: "#007AFF",
-                    boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
-                    "&:hover": { opacity: 0.8 },
+                      textTransform: "none",
+                      borderRadius: 10,
+                      height: 50,
+                      fontSize: 17,
+                      fontWeight: "bold",
+                      backgroundColor: "#007AFF",
+                      boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+                      "&:hover": { opacity: 0.8 },
                   }}
               >
-                Next
+                  Next
               </Button>
               <Button
                   variant="text"
                   onClick={handleCancel}
                   sx={{ textTransform: "none", fontSize: 15, color: "#7D7D7D" }}
               >
-                Cancel
+                  Cancel
               </Button>
-            </Stack>
+          </Stack>
 
-            <Divider sx={{ width: "100%", my: 1 }} />
-            <Stack spacing={1} direction="row" justifyContent="center">
+          <Divider sx={{ width: "100%", my: 1 }} />
+          <Stack spacing={1} direction="row" justifyContent="center">
               <Typography variant="body2">{"Don't have an account?"}</Typography>
               <Link
                   onClick={() => router.push(`/signup?id=${id}&tenant_id=${tenantId}`)}
                   sx={{ fontWeight: "bold", cursor: "pointer", color: "primary.main", fontSize: 14 }}
               >
-                Sign Up
+                  Sign Up
               </Link>
-            </Stack>
           </Stack>
-        </Paper>
-      </Container>
-    </>
+      </Stack>
+  );
+
+  return (
+    <Box sx={{ backgroundColor: "black", height: "100vh"}}>
+        <Container maxWidth={isMobile ? "xs" : "xs"}>
+            {isMobile ? (
+                <Box mt={2}>
+                    {content}
+                </Box>
+            ) : (
+                <Paper sx={{ p: 3, boxShadow: 1, borderRadius: 3 }}>
+                    {content}
+                </Paper>
+            )}
+        </Container>
+    </Box>
   );
 }
